b14ba430bb0a280f3d6e86650e12966591d8d43c
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2023-12-14 13:23:27 +0100

    working calc

---
+++ Zadanie1/script.js
@@ -0,0 +1,52 @@
+function calc() {
+    //pobieramy wartości z pól edycyjnych
+    let a = document.getElementById('a').value;
+    let b = document.getElementById('b').value;
+
+    //sprawdzamy jaką operację mamy wykonać
+    let operand = '';
+    if (document.getElementById('o1').checked)
+        operand = '+';
+    if (document.getElementById('o2').checked)
+        operand = '-';
+    if (document.getElementById('o3').checked)
+        operand = '*';
+    if (document.getElementById('o4').checked)
+        operand = '/';
+
+    //pusta zmienna na wynik:
+    let result = '';
+
+    //instrukcja switch jest idealna w sytuacji kiedy
+    //jedna ze zmiennych przyjmuje jedną z kilku
+    //dopuszczalnych wartości
+    
+    switch(operand) {
+        case '+':
+            result = a + b;
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+        default:
+            window.alert("Nieprawidłowy operator!");
+            break;
+        }
+        if(result != '') {
+            document.getElementById("result").innerHTML =
+                "Wynik działania: " + result;
+            document.getElementById("result").style.display = 
+                "block";
+        }
+}
+function addEvent() {
+    document.getElementById("calc")
+        .addEventListener("click", calc);
+}
+window.addEventListener("load", addEvent)
