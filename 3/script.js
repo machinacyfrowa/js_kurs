286d6523ef50361642f411ff564ab37a44903aa6
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2023-12-14 12:12:42 +0100

    for and do...while

---
+++ 3/script.js
@@ -0,0 +1,16 @@
+for(let i=0; i<=10; i++) {
+    //zaczynamy z licznikiem i równym 0
+    //będziemy kontynuować pętlę dopóki licznik jest < 10
+    //po każdym przebiegu pętli zwiększymy licznik o 1
+    document.write("Licznik wynosi: " + i + "<br>");
+}
+
+//do...while zawsze wykona się chociaż raz, 
+//dopiero potem sprawdza warunek
+do {
+    //losujemy liczbę całkowitą <1;6>
+    var dice = Math.ceil(Math.random() * 6);
+    //wypisujemy wylosowaną liczbę
+    document.write("Rzut kostką: " + dice + "<br>");
+    //powtarzamy dopóki nie wyrzucimy "6"
+} while(dice != 6)
