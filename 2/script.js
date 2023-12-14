29d02b8516451c53dd7bd494c358735e0b2f9b5c
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2023-12-14 11:52:20 +0100

    working?

--- 2/script.js
+++ 2/script.js
@@ -2,19 +2,27 @@ function check() {
     //pobieramy liczbę z pola edycyjnego i zapisujemy
     //do zmiennej number
     let number = document.getElementById("numberInput").value;
-    //sprawdzamy czy liczba jest parzysta
-    if(number % 2 == 0) {
-        //liczba jest parzysta
-        document.getElementById("result").innerHTML 
-                                = "Liczba jest parzysta";
+    //sprawdz czy to co dostalismy ze strony jest w ogóle liczbą
+    if (parseInt(number) != NaN) {
+        //dostaliśmy liczbę - sprawdzamy parzystość
+        //sprawdzamy czy liczba jest parzysta
+        if (number % 2 == 0) {
+            //liczba jest parzysta
+            document.getElementById("result").innerHTML
+                = "Liczba jest parzysta";
+        } else {
+            //liczba jest nieparzysta
+            document.getElementById("result").innerHTML
+                = "Liczba jest nieparzysta";
+        }
     } else {
-        //liczba jest nieparzysta
-        document.getElementById("result").innerHTML
-                                = "Liczba jest nieparzysta";
+        //dostaliśmy cos co nie jest liczbą - wyświetl błąd
+        window.alert("Podana wartość nie jest liczbą!");
     }
+
 }
 function addEvent() {
     document.getElementById("checkButton")
-                .addEventListener("click", check);
+        .addEventListener("click", check);
 }
 window.addEventListener("load", addEvent);
