cbe67de233d65d89db530e641ae8850a6f6219d7
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2023-12-16 10:11:04 +0100

    colors

--- 4/script.js
+++ 4/script.js
@@ -4,10 +4,12 @@ function validate() {
         //hasło spełnia wymóg długości
         let li = document.getElementById("passwordLength");
         li.innerHTML = "Hasło posiada co najmniej 8 znaków";
+        li.style.color = "green";
     } else {
         //hasło nie spełnia wymogu długości
         let li = document.getElementById("passwordLength");
         li.innerHTML = "Hasło nie posiada co najmniej 8 znaków";
+        li.style.color = "red";
     }
 }
 
